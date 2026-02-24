ee75f543e096604fb5a08ac18dd2e7a29cbc96b3
salvadormateo04 <salvador.mateo04@outlook.com>
2026-02-23 21:41:22 -0400

    fixed the code in general(especially the intentation)

--- Typing Master.py
+++ Typing Master.py
@@ -3,34 +3,30 @@ import random
 ListOfWords = ["anachronistic", "belligerence", "benevolence", "symphony" , "Blasphemous" , "conception" , "Pizza" , "Grandoise", "marionette", "waltzing", "Dislocated", "Enigma", "Aurora Borealis", "Diseducation", "Edutainment"]
 random.shuffle(ListOfWords)
 
-try:
-    Correct = 0
-    Mistakes = 0
+Correct = 0
+Mistakes = 0
 
-    for word in ListOfWords:
-        print("Welcome to the talking typeface game")
-        print("Type the word shown correctly and I might have a prize for you")
-        print("If you make a mistake im gonna be sad :(")
-        print("Keep in mind this game is case sensitive")
+print("Welcome to the talking typeface game")
+print("Type the word shown correctly and I might have a prize for you")
+print("If you make a mistake im gonna be sad :(")
+print("Keep in mind this game is case sensitive")
 
-        print(word)
-        Input = input("Type the world:")
+for word in ListOfWords:
+    print(word)
+    Input = input("Type the world:")
 
-        if Input == word:
-            print("Correct!")
-            Correct +=1
-        else:
-            print("You made a mistake")
-            Mistakes +=1
-            break
+    if Input == word:
+        print("Correct!")
+        Correct +=1
+    else:
+        print("You made a mistake")
+        Mistakes +=1
 
-        Accuracy = (Correct/len(ListOfWords)) * 100
+Accuracy = (Correct / len(ListOfWords)) * 100
 
-        print("Game over")
-        print("Results:")
-        print("Correct words: {Correct}")
-        print("Mistakes: {Mistakes}")
-        print("Accuracy: {Accuracy:.2f}%")
+print("\nGame over")
+print("Results:")
+print(f"Correct words: {Correct}")
+print(f"Mistakes: {Mistakes}")
+print(f"Accuracy: {Accuracy:.2f}%")
 
-except Exception as e:
-    print("An unexpected error occurred:", e)
