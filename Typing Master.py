b35a3fc94dd26bb3c221d657d0e618e813d535d2
salvadormateo04 <salvador.mateo04@outlook.com>
2026-02-23 21:03:27 -0400

    Added accuracy

--- Typing Master.py
+++ Typing Master.py
@@ -3,8 +3,6 @@ import random
 ListOfWords = ["anachronistic", "belligerence", "benevolence", "symphony" , "Blasphemous" , "conception" , "Pizza" , "Grandoise", "marionette", "waltzing", "Dislocated", "Enigma", "Aurora Borealis", "Diseducation", "Edutainment"]
 random.shuffle(ListOfWords)
 
-WordCount = 15
-
 try:
     Correct = 0
     Mistakes = 0
@@ -15,15 +13,24 @@ try:
         print("If you make a mistake im gonna be sad :(")
         print("Keep in mind this game is case sensitive")
 
-        print({word})
-        Input = input("Type the following world:")
+        print(word)
+        Input = input("Type the world:")
 
         if Input == word:
             print("Correct!")
-            Correct =+1
+            Correct +=1
+        else:
+            print("You made a mistake")
+            Mistakes +=1
+            break
 
-        if wordCount == 1:
-            print("Game over")
+        Accuracy = (Correct/len(ListOfWords)) * 100
+
+        print("Game over")
+        print("Results:")
+        print("Correct words: {Correct}")
+        print("Mistakes: {Mistakes}")
+        print("Accuracy: {Accuracy:.2f}%")
 
 except Exception as e:
     print("An unexpected error occurred:", e)
